7939975e46e1f3a3626fcbe0efc04ffdb1112af0
Benjamin Florin <benjamin.florin@gmail.com>
2016-07-25 15:51:11 -0400

    Fix Solr <title> problem in plugin

--- common/header.php
+++ common/header.php
@@ -13,7 +13,7 @@
     }
     $titleParts[] = option('site_title');
     ?>
-    <title><?php echo prepare_title(implode(' &middot; ', $titleParts)); ?></title>
+    <title><?php echo implode(' &middot; ', $titleParts); ?></title>
 
     <?php echo auto_discovery_link_tags(); ?>
 
