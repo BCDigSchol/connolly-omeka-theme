aadb8e645a4e08aa701384419b89c865a77707ac
annakijas1 <annakijas1@gmail.com>
2016-08-15 10:21:48 -0400

    Revert "Revert "Edited field names""
    This reverts commit 1bf81460a70ad88d797e8582fd2ea2b85a6a2b1b.

--- items/show.php
+++ items/show.php
@@ -1,4 +1,3 @@
-<!--Updated June 15, 2016 AK -->
 <?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'items show')); ?>
 <div id="primary">
     <h1><?php echo metadata('item', array('Dublin Core','Title')); ?></h1>
@@ -40,7 +39,7 @@
          <!-- The following prints a list of all tags associated with the item -->
     <?php if (metadata('item','has tags')): ?>
     <div id="item-tags" class="element">
-        <h3><?php echo __('Tags'); ?></h3>
+        <h3>Part of:</h3>
         <div class="element-text"><?php echo tag_string('item'); ?></div>
     </div>
     <?php endif;?>
