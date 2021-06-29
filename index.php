c9c992b92892f9cab31de78fb4b8a534f9e75557
Benjamin Florin <benjamin.florin@gmail.com>
2021-06-29 11:22:50 -0400

    Add eBook link

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
-<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
+<?php echo head(array('bodyid' => 'home', 'bodyclass' => 'two-col')); ?>
 
 <div class="intro">
-    <p>The Boston College Libraries are delighted to present <em>The Séamus Connolly Collection of Irish Music</em>. This digital collection features traditional tunes and songs collected by master fiddle player Séamus Connolly, Sullivan Artist in Residence in Irish Music at Boston College (2004 to 2015) and <a href="https://www.arts.gov/honors/heritage/fellows/s%C3%A9amus-connolly" target="_blank">National Heritage Fellow</a> (2013). Freely available, the collection offers over 330 audio recordings featuring more than 130 musicians via <a href="https://soundcloud.com/connollymusiccollection/" target="_blank">SoundCloud</a>, with accompanying stories, transcriptions, and introductory essays. </p>
+    <p>The Boston College Libraries are delighted to present <em>The Séamus Connolly Collection of Irish Music</em>.
+        This digital collection features traditional tunes and songs collected by master fiddle player Séamus Connolly,
+        Sullivan Artist in Residence in Irish Music at Boston College (2004 to 2015) and
+        <a href="https://www.arts.gov/honors/heritage/fellows/s%C3%A9amus-connolly" target="_blank">National Heritage
+            Fellow</a> (2013). Freely available, the collection offers over 330 audio recordings featuring more than 130
+        musicians via <a href="https://soundcloud.com/connollymusiccollection/" target="_blank">SoundCloud</a>, with
+        accompanying stories, transcriptions, and introductory essays. <em>A mobile-friendly
+            <a href="https://dlib.bc.edu/islandora/object/bc-ir%3A109017/datastream/PDF/view">e-book edition</a> of the collection's print content is
+            freely available for download as a PDF file.</em>
+    </p>
 
 </div>
 
@@ -45,28 +54,30 @@
 
         <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
 
-        <p><?php echo $homepageText; ?></p>
+            <p><?php echo $homepageText; ?></p>
         <?php endif; ?>
 
         <?php if (get_theme_option('Display Featured Item') == 1): ?>
-        <!-- Featured Item -->
-        <div id="featured-item" class="featured">
-            <h2><?php echo __('Featured Content'); ?></h2>
-            <?php echo random_featured_items(2); ?>
-        </div><!--end featured-item-->
+            <!-- Featured Item -->
+            <div id="featured-item" class="featured">
+                <h2><?php echo __('Featured Content'); ?></h2>
+                <?php echo random_featured_items(2); ?>
+            </div><!--end featured-item-->
         <?php endif; ?>
 
         <?php if (get_theme_option('Display Featured Collection')): ?>
-        <!-- Featured Collection -->
-        <div id="featured-collection" class="featured">
-            <h2><?php echo __('Featured Collection'); ?></h2>
-            <?php echo random_featured_collection(); ?>
-        </div><!-- end featured collection -->
+            <!-- Featured Collection -->
+            <div id="featured-collection" class="featured">
+                <h2><?php echo __('Featured Collection'); ?></h2>
+                <?php echo random_featured_collection(); ?>
+            </div><!-- end featured collection -->
         <?php endif; ?>
 
-        <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-        <!-- Featured Exhibit -->
-        <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
+        <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists(
+                'exhibit_builder_display_random_featured_exhibit'
+            )): ?>
+            <!-- Featured Exhibit -->
+            <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
         <?php endif; ?>
 
     </div>
@@ -81,12 +92,14 @@
             $recentItems = (int) $recentItems;
         endif;
         if ($recentItems):
-        ?>
-        <div id="recent-items">
-            <h2><?php echo __('Recently Added Items'); ?></h2>
-            <?php echo recent_items($recentItems); ?>
-            <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
-        </div><!--end recent-items -->
+            ?>
+            <div id="recent-items">
+                <h2><?php echo __('Recently Added Items'); ?></h2>
+                <?php echo recent_items($recentItems); ?>
+                <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __(
+                            'View All Items'
+                        ); ?></a></p>
+            </div><!--end recent-items -->
         <?php endif; ?>
 
         <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
